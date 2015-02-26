23a06141e844284204be906fb04542845c8b64d2
Ben Vandervalk <benv@bcgsc.ca>
2015-02-26 12:02:26 -0800

    R scripts: plot seq length and seq identity histograms

---
+++ evaluation/R/length-hist.r
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+data=read.delim(file('stdin'), header=FALSE)
+library(ggplot2)
+pdf(file='|cat')
+p = ggplot(data, aes(x=V1)) +
+	geom_line(stat="bin", binwidth=500) +
+	xlab("sequence length") +
+	ylab("count (log)") +
+	ggtitle("Lengths of Pseudoreads (E. coli)") +
+	scale_y_log10()
+print(p)
+dev.off()
---
+++ evaluation/R/percent-identity.r
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+data=read.delim(file('stdin'), header=FALSE)
+library(ggplot2)
+pdf(file='|cat')
+p = ggplot(data, aes(x=V1)) +
+	geom_line(stat="bin",binwidth=1) +
+	xlab("percent sequence identity") +
+	ylab("count (log)") +
+	ggtitle("Percent Sequence Identity of Pseudoreads (E. coli)") +
+	scale_y_log10() +
+	xlim(50,100) +
+	scale_x_reverse()
+print(p)
+dev.off()
